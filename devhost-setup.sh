eb2f71712456b552b3e1cf6f5fc3c508f56dff5b
Pete Johnson <petecj2@gmail.com>
2019-04-10 16:00:04 -0400

    Improved iteration

--- devhost-setup.sh
+++ devhost-setup.sh
@@ -11,34 +11,18 @@ export KUBECONFIG=~/devnet-create-faas-on-k8s/kubeconfig.yaml
 sudo snap install helm --classic
 helm init --skip-refresh --upgrade
 
+########################
+## Wait for helm to init
+########################
+
+helm repo update
+
 ## Install Minio
 helm install stable/minio --name fonkfe --set service.type=LoadBalancer,persistence.enabled=false
 
 ## Install Mongo
 helm install stable/mongodb --name fonkdb --set service.type=LoadBalancer,persistence.enabled=false,usePassword=false
 
-## Install Docker per https://docs.docker.com/install/linux/docker-ce/ubuntu/
-sudo snap install docker
-
-## Install OpenFaaS CLI
-curl -sSL https://cli.openfaas.com | sudo sh
-
-## Install OpenFaaS back end
-kubectl apply -f https://raw.githubusercontent.com/openfaas/faas-netes/master/namespaces.yml
-helm repo add openfaas https://openfaas.github.io/faas-netes/
-export PASSWORD=$(head -c 12 /dev/urandom | shasum| cut -d' ' -f1)
-
-kubectl -n openfaas create secret generic basic-auth \
---from-literal=basic-auth-user=admin \
---from-literal=basic-auth-password="$PASSWORD"
-
-helm repo update \
- && helm upgrade openfaas --install openfaas/openfaas \
-    --namespace openfaas  \
-    --set basic_auth=true \
-    --set functionNamespace=openfaas-fn
-
-export OPENFAAS_URL=http://10.10.20.208:31112
 
 ## Install OpenWhisk back end
 git clone https://github.com/apache/incubator-openwhisk-deploy-kube.git
@@ -60,12 +44,42 @@ sudo npm install -g serverless
 sudo node /usr/local/lib/node_modules/serverless/scripts/postinstall.js
 sudo chown -R $USER:$(id -gn $USER) /home/ubuntu/.config
 
-
-###################################
-## After OpenFaaS is up and running
-echo -n $PASSWORD | faas-cli login -g $OPENFAAS_URL -u admin --password-stdin
-faas-cli version
+##################################################
+## After all the OpenWhisk pods are up and Running
+##################################################
 
 ## Configure wsk
 wsk property set --apihost 10.10.20.208:31001
 wsk property set --auth 23bc46b1-71f6-4ed5-8c54-816aa4f8c502:123zO3xZCLrMN6v2BKK1dXYFpXlPkccOFqm12CdAsMgRU4VrNZ9lyGVCGuMDGIwP
+sudo apt install -y zip
+
+wsk action create create --kind nodejs:6 create.zip -i
+
+## Install Docker per https://docs.docker.com/install/linux/docker-ce/ubuntu/
+sudo snap install docker
+
+## Install OpenFaaS CLI
+curl -sSL https://cli.openfaas.com | sudo sh
+
+## Install OpenFaaS back end
+kubectl apply -f https://raw.githubusercontent.com/openfaas/faas-netes/master/namespaces.yml
+helm repo add openfaas https://openfaas.github.io/faas-netes/
+export PASSWORD=Cisco123
+
+kubectl -n openfaas create secret generic basic-auth \
+--from-literal=basic-auth-user=admin \
+--from-literal=basic-auth-password="$PASSWORD"
+
+helm repo update \
+ && helm upgrade openfaas --install openfaas/openfaas \
+    --namespace openfaas  \
+    --set basic_auth=true \
+    --set functionNamespace=openfaas-fn
+
+export OPENFAAS_URL=http://10.10.20.208:31112
+
+###################################
+## After OpenFaaS is up and running
+export PASSWORD=Cisco123
+echo -n $PASSWORD | faas-cli login -g $OPENFAAS_URL -u admin --password-stdin
+faas-cli version
